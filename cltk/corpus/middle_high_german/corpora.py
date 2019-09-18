bb4ac1c809828f648bf0b34a0f5239e67b69b8a0
clemsciences <clemsciences@aol.com>
2019-09-19 00:03:49 +0200

    Orthophonology for Old Norse: main code and test

---
+++ cltk/corpus/middle_high_german/corpora.py
@@ -0,0 +1,10 @@
+
+MIDDLE_HIGH_GERMAN_CORPORA = [
+    {
+        "encoding": "utf-8",
+        "name": "middle_high_german_corpus",
+        "location": "remote",
+        "origin": "https://github.com/cltk/middle_high_german_corpus.git",
+        "type": "text"
+    },
+]
